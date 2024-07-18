7b21958aedfc8cd2bdc849d1f0946bd481c03ed6
Arthur de Araujo Neves <arthurdearaujoneves333@gmail.com>
2024-07-18 15:50:10 -0300

    feat: Alterando status do pedido por meio da Dashboard

--- src/pages/Dashboard/dashboard.php
+++ src/pages/Dashboard/dashboard.php
@@ -12,17 +12,116 @@
     <?php
 
         include_once("../../scripts/php/VerifyLogin.php");
-        include_once("../../scripts/php/config.php");
 
-        function gettingOrders() {
+        $urlId = explode("=", $_SERVER["REQUEST_URI"]);
+        
+        if ( count($urlId) > 1 ) {
 
+            include_once("../../scripts/php/config.php");
+
+            $urlDashboard = explode("?", $urlId[0]);
+            $orderId =  $urlId[1];
             
+            $sqlQuery = mysqli_query($conexao, "SELECT * FROM pedidos WHERE id = '$orderId'");
+            $request = mysqli_num_rows($sqlQuery);
+            $order = $sqlQuery->fetch_assoc();
+
+            if ( $request == 1 ) {
+
+                if( $order["StatusDoPedido"] == 0 ) {
+
+                    mysqli_query($conexao, "UPDATE pedidos SET StatusDoPedido = 1 WHERE id = '$orderId'");
+                    
+                } else {
+
+                    mysqli_query($conexao, "UPDATE pedidos SET StatusDoPedido = 0 WHERE id = '$orderId'");
+                    
+
+                }
+
+                header("Location: $urlDashboard[0]");
+                
+
+            };
 
         };
-
+        
     ?>
 
-    <p>Dashboard</p>
+    <p class="Logo">Pedidos<span class="LogoOnline">Online</span></p>
+
+    <div class="dashboard">
+
+        <?php
+
+            include_once("../../scripts/php/config.php");
+            $sqlQuery = mysqli_query($conexao, "SELECT * FROM pedidos");
+            $request = mysqli_num_rows($sqlQuery);
+
+            if ( $request > 0 ) {
+
+                for ( $i = 0; $i < $request; $i++) {
+
+                    $order = $sqlQuery->fetch_assoc();
+
+                    $nome = $order["Nome"];
+                    $endereco = $order["Endereco"];
+                    $pedido = $order["Pedido"];
+                    $horario = $order["Horario"];
+                    $StatusDoPedido = $order["StatusDoPedido"];
+                    $StatusClass = "naoAtendido";
+                    $id = $order["id"];
+                    $currentPage = $_SERVER["PHP_SELF"];
+
+                    if ( $StatusDoPedido == 0 ) {
+
+                        $StatusDoPedido = "Não atendido";
+
+                    } else {
+
+                        $StatusDoPedido = "Atendido";
+                        $StatusClass = "atendido";
+                    };
+
+                    echo "
+
+                        <div class='pedido'>
+
+                            <div><p class='title'>Nome</p><p>$nome</p></div>
+                            <div><p class='title'>Endereço</p><p>$endereco</p></div>
+                            <div><p class='title'>Pedido</p><p>$pedido</p></div>
+                            <div><p class='title'>Pedido feito</p><p>$horario</p></div>
+                            <div><p class='title'>Status</p><p class='$StatusClass'>$StatusDoPedido</p></div>
+                            <a href='$currentPage?id=$id'>ALTERAR STATUS</a>
+                            
+                        </div>
+
+                    ";
+                    echo "</br>";
+
+                };
+
+            } else {
+
+                echo("Nenhum pedido feito");
+
+            };
+
+            if ( isset($_REQUEST["reload"]) ) {
+
+                header("Refresh:0");
+                
+            };
+
+        ?>
+
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+
+            <input type="submit" value="Puxar dados" name="reload"  />
+
+        </form>
+
+    </div> <!--Pedidos-->
     
 </body>
 </html>
